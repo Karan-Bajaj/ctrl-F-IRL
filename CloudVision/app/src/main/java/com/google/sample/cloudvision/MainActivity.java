2ea943782d673462b67947a14f65e204c146b3e9
Jack Xu <projectbyjx@gmail.com>
2018-02-25 15:37:33 -0500

    Delete API keys

--- CloudVision/app/src/main/java/com/google/sample/cloudvision/MainActivity.java
+++ CloudVision/app/src/main/java/com/google/sample/cloudvision/MainActivity.java
@@ -60,7 +60,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String CLOUD_VISION_API_KEY = "AIzaSyAZAEfpwX-NL1GjJopLrrQdoHjJnpsCjGU";
+    private static final String CLOUD_VISION_API_KEY = "(-YOUR-API-KEY-)";
     public static final String FILE_NAME = "temp.jpg";
     private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
     private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
